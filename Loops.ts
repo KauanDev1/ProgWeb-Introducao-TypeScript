7ffe4079866e7af2f48f7f6ebf0d746984ce9535
KauanDev1 <k4uangnom0@gmail.com>
2025-06-04 17:08:46 -0400

    commit

---
+++ Loops.ts
@@ -0,0 +1,19 @@
+for (let i = 0; i < 3; i++){
+    console.log(i)
+}
+
+let j = 0;
+
+while(j < 3){
+    console.log(j)
+    j++;
+}
+
+let p = 0;
+do {
+    console.log(y);
+    p++;
+} while ( p < 3) 
+
+
+
